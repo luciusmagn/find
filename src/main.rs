eb5803713e0683f6eed50af328334570ea5a8131
07devAdvb <davidvivarbogonez@gmail.com>
2024-10-10 22:08:07 +0200

    First working version after refactor

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,41 @@
-use std::time::Instant;
-use clap::{Arg, Command};
-use colored::*;
 use std::path::Path;
+
+use clap::Parser;
 
 mod finder;
 
+/// Find files by name in a specified directory
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The name of the file to search for
+    #[arg(short, long)]
+    filename: String,
+
+    /// The directory to search
+    #[arg(short, long)]
+    dir: String,
+    
+    /// Show the time elapsed for the search
+    #[arg(short, long)]
+    time: bool,
+}
+
 fn main() {
-    // Define the CLI arguments using `clap`
-    let matches = Command::new("File Finder")
-        .version("1.0")
-        .author("Your Name")
-        .about("Searches for a file in a given directory")
-        .arg(Arg::new("directory")
-            .short('d')
-            .long("directory")
-            .value_name("DIRECTORY")
-            .help("The directory to search in")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::new("filename")
-            .short('f')
-            .long("filename")
-            .value_name("FILENAME")
-            .help("The name of the file to search for")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::new("benchmark")
-            .short('b')
-            .long("benchmark")
-            .help("Show the time taken for the search"))
-        .get_matches();
+    let args = Args::parse();
+    let filename = &args.filename;
+    let dir = &args.dir;
+    let time = args.time;
 
-    // Get the directory and filename arguments
-    let directory = matches.value_of("directory").unwrap();
-    let filename = matches.value_of("filename").unwrap();
-    let benchmark = matches.is_present("benchmark");
+    let start = std::time::Instant::now();
+    let results = finder::search_files(&Path::new(dir), filename);
+    let elapsed = start.elapsed();
 
-    // Start the benchmark timer if the flag is set
-    let start_time = Instant::now();
-
-    // Perform the search
-    let results = finder::search_files(Path::new(directory), filename);
-
-    // Output results
-    if results.is_empty() {
-        println!("{}", "No files found.".red());
-    } else {
-        println!("{}", "Files found:".green());
-        for result in results {
-            println!("{}", result.display().to_string().yellow());
-        }
+    if time {
+        println!("Time elapsed: {:?}", elapsed);
     }
 
-    // Show benchmark if the flag was set
-    if benchmark {
-        let duration = start_time.elapsed();
-        println!(
-            "{}: {} seconds",
-            "Search completed in".blue(),
-            format!("{:.2}", duration.as_secs_f64()).yellow()
-        );
+    for result in results {
+        println!("{}", result.display());
     }
 }
